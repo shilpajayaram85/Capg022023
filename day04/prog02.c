43fc327f1fe39bf449a1a75856db95c1d32a36aa
shilpajayaram85 <shilpa85.jayaram@gmail.com>
2023-02-10 16:12:51 +0530

    day04

---
+++ day04/prog02.c
@@ -0,0 +1,55 @@
+/*
+ i/o statements
+  printf("Control string, vargs");
+  scanf("Control string", vargs address);
+  %c-single character
+  %d-integer
+  %f-float
+  %u-unsigned integer
+  %lf -double
+
+*/
+
+#include<stdio.h>
+
+
+int main()
+{
+/*	int i=10123;
+	float f=11.11;
+	double d =12.22;
+	char c='X';
+	unsigned ui=100; */
+
+	/*int i;
+	float f;
+	double d;
+	char c;
+	unsigned ui;*/
+	//int l= printf("Hello shilpa\n");
+	int a=10;
+	printf("printed characters %d\n",printf("Hello shilpa\n"),a,a,a);
+	/*printf("ENTER INTEGER FLOAT DOUBLE UNSIGNED AND CHARACTER VALUES\b\n");
+	printf("\a");
+	scanf("%3d",&i);
+	scanf("%f",&f);
+	scanf("%lf",&d);
+	scanf("%u",&ui);
+	scanf(" ");
+	scanf("%c",&c);
+	
+	printf("\ni=%5d",i);
+	printf("\nf=%5f",f);
+	printf("\nd=%lf",d);
+	printf("\nc=%c ASCII value=%d",c,c);
+	printf("\nui=%u",ui);
+
+	printf("\ni=%u",&i);
+        printf("\nf=%u",&f);
+        printf("\nd=%u",&d);
+        printf("\nc=%u",&c);
+        printf("\nui=%u\n",&ui);*/
+	
+
+	return 0;
+}	
